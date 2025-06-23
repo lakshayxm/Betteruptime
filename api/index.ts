6c4d68bffb8efe89db21020968122cd0cfe613fa
Lakshay Malhotra <lakshay.sbp@gmail.com>
2025-06-23 14:32:17 -0400

    added schema

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
 const app = express();
+import { prismaClient } from "store/client";
 
 app.post("/website", (req, res) => {});
 
 app.get("/status/:websiteId", (req, res) => {});
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3001);
